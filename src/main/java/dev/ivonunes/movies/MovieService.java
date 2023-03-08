e38d88de122dfecfe7b7f9327bc21520c54812d4
Ivo Nunes <ivonunes@Ivo-MacBookPro-14.local>
2023-03-08 16:31:41 +0000

    Single Movie

--- src/main/java/dev/ivonunes/movies/MovieService.java
+++ src/main/java/dev/ivonunes/movies/MovieService.java
@@ -1,9 +1,11 @@
 package dev.ivonunes.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,4 +14,9 @@ public class MovieService {
   public List<Movie> allMovies(){
     return movieRepository.findAll(); //method described in Mongo Repository
   }
+
+  public Optional<Movie> singleMovie(ObjectId id){
+    return movieRepository.findById(id);
+  }
+
 }
